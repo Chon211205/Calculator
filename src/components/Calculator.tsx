6bb95a9a2e8c455a6df3e2dc7b89a1401b229d98
Chon211205 <lee241203@uvg.edu.gt>
2026-05-05 20:55:13 -0600

    svg personal added

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -3,21 +3,12 @@ import { Display } from './Display'
 import { Keypad } from './Keypad'
 import { useCalculator } from '../hooks/useCalculator'
 
-const buttons = [
-  '7', '8', '9', '+',
-  '4', '5', '6', '-',
-  '1', '2', '3', '*',
-  '0', '.', '=', '/',
-  '%', '+/-'
-]
+const buttons = ['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '*', '0', '.', '=', '/', '%', '+/-']
 
 export function Calculator() {
   const [display, setDisplay] = useState('0')
   const calculator = useCalculator()
-
-  function handlePress(value: string) {
-    setDisplay(calculator.press(display, value))
-  }
+  const handlePress = (value: string) => setDisplay(calculator.press(display, value))
 
   return (
     <section className="calculator">
